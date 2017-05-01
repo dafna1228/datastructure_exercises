3c514d9e1e33b57283d3b72771db7d93f25b05d5
dafna <dafna1228@gmail.com>
2017-05-01 13:19:40 +0300

    finished the Tank class

--- ex2/Tank.java
+++ ex2/Tank.java
@@ -1,5 +1,15 @@
 
 public class Tank implements Comparable<Tank>{
+
+	public static void main(String[] args) {
+		Tank t1 = new Tank("bA");
+		Tank t2 = new Tank("DBB");
+		Tank t22 = new Tank("dbB");
+
+		System.out.println(t1.compareTo(t2));
+		System.out.println(t2.compareTo(t1));
+		System.out.println(t22.compareTo(t2));
+	}
 	
 	private String serialNumber;
 	
@@ -9,7 +19,7 @@ public class Tank implements Comparable<Tank>{
 	 * @param serialNumber - the serial number of the tank
 	 */
 	public Tank(String serialNumber){
-		//your code comes here
+		this.serialNumber = serialNumber;
 	}
 	
 	/**
@@ -22,8 +32,7 @@ public class Tank implements Comparable<Tank>{
 	 * 		   less than, equal to, or greater than the serial number of the other tank.
 	 */
 	public int compareTo(Tank other) {
-		//your code comes here
-		return 0;
+		return this.serialNumber() - other.serialNumber();
 	}
 	
 	/**
@@ -33,9 +42,29 @@ public class Tank implements Comparable<Tank>{
 	 * @return the integer represented by the serial number.
 	 */
 	public int serialNumber(){
-		//your code comes here
+		int result = 0;
+		for (int i = 0; i < serialNumber.length(); i++){
+			int num = charToInt(serialNumber.charAt(i));
+			// sum the value of a letter
+			result += num * (int) Math.pow(26,(serialNumber.length() - 1 - i));
+		}
+		return result;
+	}
+
+	// convert a char to int
+	public int charToInt(char ch){
+		// if capital letter
+		if ((int) ch >= 65 && (int) ch <= 90) {
+			return (int) ch - 65;
+		// if lowercase letter
+		} else if ((int) ch >= 97 && (int) ch <= 122) {
+			return  (int) ch - 97;
+		}
 		return 0;
 	}
 
+	public String toString(){
+		return serialNumber + " " + this.serialNumber();
+	}
 	
 }
