fbb2ad81e7a0de6b0612039692f5696139e76684
dafna <dafna1228@gmail.com>
2017-05-02 20:07:17 +0300

    started simulation. it only runs once, and the tank numbers are not uniqe

--- ex2/Simulation.java
+++ ex2/Simulation.java
@@ -15,12 +15,47 @@ public class Simulation {
 			System.out.print(tankNums[i] + " ");
 		}
 
+        TankEstimator te = new TankEstimator();
+        for (int i = 0; i < k; i++){
+            te.captureTank(new Tank(intTo26(tankNums[i])));
+        }
+        System.out.println();
+        System.out.println(te.estimateProduction());
+		/*
+
 		// create an estimation for these captured tanks
+
+		        System.out.println();
+
+		String[] tankNames = new String[k];
+        for (int i = 0; i < k; i++){
+            tankNames[i] = intTo26(tankNums[i]);
+        }
+
+        for (int i = 0; i < k; i++){
+            System.out.print(tankNames[i] + " ");
+        }
+
+        System.out.println();
+
+        Tank[] tanklist = new Tank[k];
+        for (int i = 0; i < k; i++){
+            tanklist[i] = new Tank(tankNames[i]);
+            System.out.print(tanklist[i].serialNumber() + " ");
+        }
+		*/
 	}
 
 	// translate the ints to a base 26 string
-	public static String[] intTo26 (int[] arr) {
-		String[] tankStrings = new String[arr.length];
+	public static String intTo26 (int num) {
+		String tankStrings = "";
+
+		int length = String.valueOf(num).length();
+        for (int i = 0; i < length; i++){
+            tankStrings = (char) ((num % 26) + 97) + tankStrings;
+            num = num / 26;
+            //System.out.println(num / 26);
+        }
 
 		return tankStrings;
 	}
