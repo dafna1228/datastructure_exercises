d117a7403643bde3afbc225a035dbdcc360521d5
dafna <dafna1228@gmail.com>
2017-05-30 21:31:10 +0300

    new ex4

---
+++ ex4/Sorting.java
@@ -0,0 +1,204 @@
+import java.util.Random;
+
+public class Sorting {
+
+	final static int SELECTION_VS_QUICK_LENGTH = 12;
+	final static int MERGE_VS_QUICK_LENGTH = 15;
+	final static int MERGE_VS_QUICK_SORTED_LENGTH = 12;
+	final static int SELECT_VS_MERGE_LENGTH = 16;
+	final static double T = 600.0;
+	/**
+	 * Sorts a given array using the quick sort algorithm.
+	 * At each stage the pivot is chosen to be the rightmost element of the subarray.
+	 * 
+	 * Should run in average complexity of O(nlog(n)), and worst case complexity of O(n^2)
+	 * 
+	 * @param arr - the array to be sorted
+	 */
+	public static void quickSort(double[] arr){
+		//your code comes here
+	}
+	
+	/**
+	 * Sorts a given array using the merge sort algorithm.
+	 * 
+	 * Should run in complexity O(nlog(n)) in the worst case.
+	 * 
+	 * @param arr - the array to be sorted
+	 */
+	public static void mergeSort(double[] arr){
+		//your code comes here
+	}
+	
+
+	/**
+	 * finds the i'th order statistic of a given array.
+	 * 
+	 * Should run in complexity O(n) in the worst case.
+	 * 
+	 * @param arr - the array.
+	 * @param i - a number between 0 and arr.length - 1.
+	 * @return the number which would be at index i, if the array was to be sorted
+	 */
+	public static double select (double[] arr, int i){
+		//your code comes here
+		return 0.0;
+	}
+	
+	/**
+	 * Sorts a given array using the selection sort algorithm.
+	 * 
+	 * Should run in complexity O(n^2) in the worst case.
+	 * 
+	 * @param arr - the array to be sorted
+	 */
+	public static void selectionSort(double[] arr){
+		//your code comes here
+	}
+	
+	public static void main(String[] args) {
+		selectionVsQuick();
+		mergeVsQuick();
+		mergeVsQuickOnSortedArray();
+		selectVsMerge();
+	}
+	
+	/**
+	 * Compares the selection sort algorithm against quick sort on random arrays
+	 */
+	public static void selectionVsQuick(){
+		double[] quickTimes = new double[SELECTION_VS_QUICK_LENGTH];
+		double[] selectionTimes = new double[SELECTION_VS_QUICK_LENGTH];
+		long startTime, endTime;
+		Random r = new Random();
+		for (int i = 0; i < SELECTION_VS_QUICK_LENGTH; i++) {
+			long sumQuick = 0;
+			long sumSelection = 0;
+			for(int k = 0; k < T; k++){
+				int size = (int)Math.pow(2, i);
+				double[] a = new double[size];
+				double[] b = new double[size];
+				for (int j = 0; j < a.length; j++) {
+					a[j] = r.nextGaussian() * 5000;
+					b[j] = a[j];
+				}
+				startTime = System.currentTimeMillis();
+				quickSort(a);
+				endTime = System.currentTimeMillis();
+				sumQuick += endTime - startTime;
+				startTime = System.currentTimeMillis();
+				selectionSort(b);
+				endTime = System.currentTimeMillis();
+				sumSelection += endTime - startTime;
+			}
+			quickTimes[i] = sumQuick/T;
+			selectionTimes[i] = sumSelection/T;
+		}
+		Plotter.plot("quick sort", quickTimes, "selection sort", selectionTimes);
+	}
+	
+	/**
+	 * Compares the merge sort algorithm against quick sort on random arrays
+	 */
+	public static void mergeVsQuick(){
+		double[] quickTimes = new double[MERGE_VS_QUICK_LENGTH];
+		double[] mergeTimes = new double[MERGE_VS_QUICK_LENGTH];
+		long startTime, endTime;
+		Random r = new Random();
+		for (int i = 0; i < MERGE_VS_QUICK_LENGTH; i++) {
+			long sumQuick = 0;
+			long sumMerge = 0;
+			for (int k = 0; k < T; k++) {
+				int size = (int)Math.pow(2, i);
+				double[] a = new double[size];
+				double[] b = new double[size];
+				for (int j = 0; j < a.length; j++) {
+					a[j] = r.nextGaussian() * 5000;
+					b[j] = a[j];
+				}
+				startTime = System.currentTimeMillis();
+				quickSort(a);
+				endTime = System.currentTimeMillis();
+				sumQuick += endTime - startTime;
+				startTime = System.currentTimeMillis();
+				mergeSort(b);
+				endTime = System.currentTimeMillis();
+				sumMerge += endTime - startTime;
+			}
+			quickTimes[i] = sumQuick/T;
+			mergeTimes[i] = sumMerge/T;
+		}
+		Plotter.plot("quick sort", quickTimes, "merge sort", mergeTimes);
+	}
+
+	/**
+	 * Compares the merge sort algorithm against quick sort on pre-sorted arrays
+	 */
+	public static void mergeVsQuickOnSortedArray(){
+		double[] quickTimes = new double[MERGE_VS_QUICK_SORTED_LENGTH];
+		double[] mergeTimes = new double[MERGE_VS_QUICK_SORTED_LENGTH];
+		long startTime, endTime;
+		Random r = new Random();
+		for (int i = 0; i < MERGE_VS_QUICK_SORTED_LENGTH; i++) {
+			long sumQuick = 0;
+			long sumMerge = 0;
+			for (int k = 0; k < T; k++) {
+				int size = (int)Math.pow(2, i);
+				double[] a = new double[size];
+				double[] b = new double[size];
+				for (int j = 0; j < a.length; j++) {
+					a[j] = j;
+					b[j] = j;
+				}
+				startTime = System.currentTimeMillis();
+				quickSort(a);
+				endTime = System.currentTimeMillis();
+				sumQuick += endTime - startTime;
+				startTime = System.currentTimeMillis();
+				mergeSort(b);
+				endTime = System.currentTimeMillis();
+				sumMerge += endTime - startTime;
+			}
+			quickTimes[i] = sumQuick/T;
+			mergeTimes[i] = sumMerge/T;
+		}
+		Plotter.plot("quick sort on sorted array", quickTimes, "merge sort on sorted array", mergeTimes);
+	}
+
+	/**
+	 * Compares the select algorithm against sorting an array.
+	 */
+	public static void selectVsMerge(){
+		double[] mergeTimes = new double[MERGE_VS_QUICK_LENGTH];
+		double[] selectTimes = new double[MERGE_VS_QUICK_LENGTH];
+		long startTime, endTime;
+		double x;
+		Random r = new Random();
+		for (int i = 0; i < MERGE_VS_QUICK_LENGTH; i++) {
+			long sumMerge = 0;
+			long sumSelect = 0;
+			for (int k = 0; k < T; k++) {
+				int size = (int)Math.pow(2, i);
+				double[] a = new double[size];
+				double[] b = new double[size];
+				for (int j = 0; j < a.length; j++) {
+					a[j] = r.nextGaussian() * 5000;
+					b[j] = a[j];
+				}
+				int index = (int)(Math.random() * size);
+				startTime = System.currentTimeMillis();
+				mergeSort(a);
+				x = a[index];
+				endTime = System.currentTimeMillis();
+				sumMerge += endTime - startTime;
+				startTime = System.currentTimeMillis();
+				x = select(b, index);
+				endTime = System.currentTimeMillis();
+				sumSelect += endTime - startTime;
+			}
+			mergeTimes[i] = sumMerge/T;
+			selectTimes[i] = sumSelect/T;
+		}
+		Plotter.plot("merge sort and select", mergeTimes, "select", selectTimes);
+	}
+}
