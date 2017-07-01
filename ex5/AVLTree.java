73162b7143699a2ea2a02d4dea113f9ec91125af
dafna <dafna1228@gmail.com>
2017-07-01 20:00:28 +0300

    finished!

--- ex5/AVLTree.java
+++ ex5/AVLTree.java
@@ -1,43 +1,48 @@
 //package HW5;
 
 public class AVLTree {
-    public static void main(String[] args) {
-        AVLTree tree = new AVLTree();
-        //String[] ids = new String[]{"b", "a", "c", "d", "e", "f", "g", "h"};
-        //for (int i = 0; i< 3; i++){
-        //    tree.insert(new Tank(ids[i]));
-        //}
 
-        /*
-        tree.root = new AVLNode(new Tank("l"));
-        tree.root.left = new AVLNode(new Tank("c"));
-        tree.root.left.parent = tree.root;
-        tree.root.right = new AVLNode(new Tank("o"));
-        tree.root.right.parent = tree.root;
-        tree.root.right.right = new AVLNode(new Tank("r"));
-        tree.root.right.right.parent = tree.root.right;
-       // tree.root.right.right.right = new AVLNode(new Tank("z"));
-       // tree.root.right.right.right.parent = tree.root.right.right;
-        tree.root.right.left = new AVLNode(new Tank("n"));
-        tree.root.right.left.parent = tree.root.right;
-        tree.root.height = 2; tree.root.left.height = 1; tree.root.right.height = 1; tree.root.right.right.height = 0; tree.root.right.left.height = 0;
-        tree.size = 5;
-*/
+    public static void main(String[] args){
+        AVLTree Test = new AVLTree();
+        Tank Tank1 = new Tank("a");
+        Tank Tank2 = new Tank("b");
+        Tank Tank3 = new Tank("c");
+        Tank Tank4 = new Tank("d");
+        Tank Tank5 = new Tank("e");
+        Tank Tank6 = new Tank("f");
+        Tank Tank7 = new Tank("g");
+        Tank Tank8 = new Tank("h");
+        Tank[] ordered = new Tank[7];
 
-        tree.insert(new Tank("l"));
-        tree.insert(new Tank("c"));
-        tree.insert(new Tank("o"));
-        tree.insert(new Tank("r"));
-        tree.insert(new Tank("z"));
-        Tank[] sorted = tree.inorder();
-        for (int i = 0; i< tree.size; i++) {
-            System.out.println(sorted[i]);
+        Test.insert(Tank4);
+        Test.insert(Tank5);
+        Test.insert(Tank6);
+        Test.insert(Tank2);
+        Test.insert(Tank3);
+        Test.insert(Tank1);
+        Test.insert(Tank8);
+        Test.insert(Tank7);
+
+//        Test.insert(Tank7);
+//        Test.insert(Tank8);
+//        Test.insert(Tank1);
+//        Test.insert(Tank3);
+//        Test.insert(Tank2);
+//        Test.insert(Tank6);
+//        Test.insert(Tank5);
+//        Test.insert(Tank4);
+
+        System.out.println();
+        System.out.println("PRINTING");
+        ordered = Test.inorder();
+        for (int i =0; i< ordered.length; i++){
+            System.out.println(ordered[i]);
         }
     }
 
-
     AVLNode root; 	// The tree root.
     int size;		// The size of the tree.
+
    
     /**
      * Construct an empty tree.
@@ -54,40 +59,35 @@ public class AVLTree {
      * @param t the tank to insert.
      */
     public void insert(Tank t) {
-        System.out.println("inserting tank: " + t.toString());
         AVLNode node = insert_bst(t, this);
-        //System.out.println("tank height: " + node.height);
+        // balance all the new node's parents
         AVLNode parent = node.parent;
         while (parent != null){
-            System.out.println("balancing tank: " + parent.t.toString());
-            System.out.println("BF: " +BalanceFactor(parent) );
             if (Math.abs(BalanceFactor(parent)) > 1) {
                 //Left Left Case
                 if (BalanceFactor(parent) > 1 && t.compareTo(parent.left.t) < 0) {
-                    System.out.println("Left Left Case on: " + parent.t.toString());
                     rightRotate(parent);
                 }
+                //Right Right Case
                 if (BalanceFactor(parent) < -1 && t.compareTo(parent.right.t) > 0) {
-                    System.out.println("Right Right Case on: " + parent.t.toString());
                     leftRotate(parent);
                 }
                 // Left Right Case
-
                 if (BalanceFactor(parent) > 1 && t.compareTo(parent.left.t) > 0) {
-                    System.out.println("Left Right Case on: " + parent.t.toString());
-                    rightLeftRotate(parent);
+                    parent.left = leftRotate(parent.left);
+                    rightRotate(parent);
                 }
                 // Right Left Case
-
-                if (BalanceFactor(parent) < -1 && t.compareTo(parent.right.t) < 0) {
-                    System.out.println("Right Left Case on: " + parent.t.toString());
-                    leftRightRotate(parent);
+                if (BalanceFactor(parent) < -1 && t.compareTo(parent.right.t) < 0 ) {
+                    parent.right = rightRotate(parent.right);
+                    leftRotate(parent);
                 }
             }
             parent = parent.parent;
         }
     }
 
+    // insert using the BST insert algorithm, might create an unbalanced tree
     public AVLNode insert_bst(Tank n, AVLTree T) {
         AVLNode node = new AVLNode(n);
         AVLNode parent = null;
@@ -102,118 +102,101 @@ public class AVLTree {
             }
         }
         node.parent = parent;
+
         if (parent == null){
             // tree is empty
             T.root = node;
             node.height = 0;
             this.size++;
             return node;
-
+            // else, update the parents of new node
         } else if (node.t.compareTo(parent.t)< 0){
             parent.left = node;
         } else {
             parent.right = node;
         }
 
-        // update height
+        // update height for all the new node's parents
         parent = node.parent;
         node.height = 0;
         while (parent != null){
-            parent.height +=1;
+            parent.height = Math.max(getHeight(parent.left), getHeight(parent.right)) + 1;
             parent = parent.parent;
         }
         this.size++;
         return node;
     }
 
-    public void leftRotate(AVLNode x){
+    // Left rotate
+    public AVLNode leftRotate(AVLNode x){
         AVLNode z = x.right;
         AVLNode t23 = z.left;
         x.right = t23;
-        t23.parent = x;
+        if (t23 != null ) {
+            t23.parent = x;
+        }
+        // switch locations and update parent/ child
         z.parent = x.parent;
-        if (x.parent.left == x ) {
-            z.parent.left = z;
-        } else {
-            z.parent.right = z;
+        if (x.parent != null) {
+            if (x.parent.left == x) {
+                z.parent.left = z;
+            } else {
+                z.parent.right = z;
+            }
         }
         z.left = x;
         x.parent = z;
 
-        x.height = Math.max(x.left.height, x.right.height) + 1;
-        z.height = Math.max(z.left.height, z.right.height) + 1;
+        if (z.parent == null){
+            this.root = z;
+        }
+
+        // update heights
+        x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
+        z.height = Math.max(getHeight(z.left), getHeight(z.right)) + 1;
+        return z;
     }
 
-    public void rightRotate(AVLNode x){
+    // Right Right rotate
+    public AVLNode rightRotate(AVLNode x){
         AVLNode z = x.left;
         AVLNode t23 = z.right;
         x.left = t23;
-        t23.parent = x;
+        if (t23 != null ) {
+            t23.parent = x;
+        }
+        // switch locations and update parent/ child
         z.parent = x.parent;
-        if (x.parent.left == x ) {
-            z.parent.left = z;
-        } else {
-            z.parent.right = z;
+        if (x.parent != null) {
+            if (x.parent.left == x) {
+                z.parent.left = z;
+            } else {
+                z.parent.right = z;
+            }
         }
         z.right = x;
         x.parent = z;
-
-        x.height = Math.max(x.left.height, x.right.height) + 1;
-        z.height = Math.max(z.left.height, z.right.height) + 1;
+        if (z.parent == null){
+            this.root = z;
+        }
+        // update height
+        x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
+        z.height = Math.max(getHeight(z.left), getHeight(z.right)) + 1;
+        return z;
     }
 
-    public void rightLeftRotate(AVLNode x){
-        AVLNode z = x.right;
-        AVLNode y = z.left;
-        AVLNode t3 = y.right;
-        z.left = t3;
-        t3.parent = z;
-        y.right = z;
-        z.parent = y;
-
-        AVLNode t2 = y.left;
-        x.right = t2;
-        t2.parent = x;
-        y.left = x;
-        x.parent = y;
-
-        x.height = Math.max(x.left.height, x.right.height) + 1;
-        z.height = Math.max(z.left.height, z.right.height) + 1;
-        y.height = Math.max(y.left.height, y.right.height) + 1;
-    }
-
-
-    public void leftRightRotate(AVLNode x){
-        AVLNode z = x.left;
-        AVLNode y = z.right;
-        AVLNode t3 = y.left;
-        z.right = t3;
-        t3.parent = z;
-        y.left = z;
-        z.parent = y;
-
-        AVLNode t2 = y.right;
-        x.left = t2;
-        t2.parent = x;
-        y.right = x;
-        x.parent = y;
-
-        x.height = Math.max(x.left.height, x.right.height) + 1;
-        z.height = Math.max(z.left.height, z.right.height) + 1;
-        y.height = Math.max(y.left.height, y.right.height) + 1;
-    }
-
+    // return the height of the node. if it does not exist, return -1
     public int getHeight(AVLNode n) {
         if (n != null) {
             return n.height;
         }
-        return 0;
+        return -1;
     }
 
-public int BalanceFactor( AVLNode node){
-     //   System.out.println("left: "+getHeight(node.left)+ ", right: "+getHeight(node.right));
+    // return the difference of heights between left and right subtrees of the node
+    public int BalanceFactor( AVLNode node){
         return getHeight(node.left) - getHeight(node.right);
-}
+    }
     /**
      * Search for a tank in the tree.
      * 
@@ -224,6 +207,7 @@ public int BalanceFactor( AVLNode node){
         AVLNode index = this.root;
 
         while ( index != null) {
+            // compare the node and walk down the tree
             if(index.t.compareTo(t) < 0){
                 index = index.right;
             } else if(index.t.compareTo(t) > 0) {
@@ -246,7 +230,10 @@ public int BalanceFactor( AVLNode node){
         int i = 0;
         Tank[] tankArr = new Tank[size];
         AVLNode node = this.root;
-        fill_array(node, tankArr, i);
+        // if the tree is not empty, do a recursive inorder and write results to the array
+        if (this.root != null) {
+            fill_array(node, tankArr, i);
+        }
         return tankArr;
     }
 
@@ -258,7 +245,8 @@ public int BalanceFactor( AVLNode node){
         if (root.right != null) {
             pos = fill_array(root.right, array, pos);
         }
-        return pos; // return the last position filled in by this invocation
+        // return the last position filled in by this invocation
+        return pos;
     }
 
 }
